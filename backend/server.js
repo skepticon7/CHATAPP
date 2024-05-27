4b0c8abe3e6bcea250c9c7423e6329bc39f3f187
unknown <me@here.com>
2024-05-27 22:24:22 +0100

    again last one

--- backend/server.js
+++ backend/server.js
@@ -4,14 +4,21 @@ const ConnectToDb = require("./DB/ConnectToDb");
 const authRoutes = require("./ROUTES/auth.routes.js");
 const MessageRoute = require("./ROUTES/messages.routes.js");
 const UserRoute  = require("./ROUTES/user.routes.js");
-const RouterProtection = require("./MIDDLWARE/ProtectionMiddlware.js");
 const cookieParser = require("cookie-parser"); 
 const {app, server} = require("./SOCKET/socket.js");
+const path = require("path");
 
-
-dotenv.config({path : "../.env"});
+dotenv.config();
 
 ConnectToDb();
+
+
+app.use(express.static(path.join(__dirname ,"/frontend/my-chat-app/dist")));
+
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname , "frontend" , "my-chat-app","dist" , "index.html"));
+})
+
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api",authRoutes);
