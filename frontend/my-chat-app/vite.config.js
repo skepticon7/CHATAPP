7eaa09887633472add7943bda506dfcfac0d8df0
unknown <me@here.com>
2024-05-28 23:06:35 +0100

    6aaaalah

--- frontend/my-chat-app/vite.config.js
+++ frontend/my-chat-app/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port:3000,
     proxy:{
       "/api" : {
-        target:"http://localhost:5050",
+        target:"https://chatify-pod8.onrender.com",
       }
     }
   },
